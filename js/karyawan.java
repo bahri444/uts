06ef34c95d2c146ed913aaa7e9b0b05d3b016a8e
s43epulb4hry <bahrysaipul266@gmail.com>
2021-11-04 19:49:34 +0800

    fileuts

--- js/karyawan.java
+++
@@ -1,6 +0,0 @@
-class karyawan extends ktp {
-
-    public karyawan() {
-        System.out.println("ini di kelas karyawan");
-    }
-}
--- js/ktp.java
+++
@@ -1,8 +0,0 @@
-class ktp {
-    protected String nama;
-    protected String alamat;
-
-    public ktp() {
-        System.out.println("ini berada di kelas induk");
-    }
-}
